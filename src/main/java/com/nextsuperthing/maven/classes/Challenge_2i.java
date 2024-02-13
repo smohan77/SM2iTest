9340caf58b799480d7d87e2214a0cc6a6366fd9a
smohan77 <sbhagavathi@gmail.com>
2024-02-13 20:07:16 +0000

    output with fixed array

--- src/main/java/com/nextsuperthing/maven/classes/Challenge_2i.java
+++ src/main/java/com/nextsuperthing/maven/classes/Challenge_2i.java
@@ -1,19 +1,26 @@
 package com.nextsuperthing.maven.classes;
 
-public class Challenge_2i {
-	
-	 public static void main(String args[]) {
-		 
-		 //int pairCount = findPairsToMakeSum(sortedArray, X);
-		 
-	 }
-	 
-	 public static int findSolution(int[] arrayOfNumbers, int sumToMake) {
-		 int val=0;
-		 
-		 return val;
-		 
-	 }
-	 
 
-}
+public class Challenge_2i {
+	public static void main(String[] args) {
+		int highest = 0;
+		String[] inputArr = {"x4y5z", "ab2c7","gf5gf7fg5ff5"};
+		for( int k=0;k<inputArr.length;k++) {
+		String str = inputArr[k];
+		int sum =0;		
+		for(int i =0;i<str.length();i++) {
+			if(str.charAt(i)>='0' && str.charAt(i)<='9') {
+				
+				sum+=(str.charAt(i)-'0');
+				
+				if(sum>highest) {
+					highest=sum;
+					
+				}
+			}
+		}
+		
+	}
+		System.out.println("Print highest total value:"+ highest);
+		
+}}
