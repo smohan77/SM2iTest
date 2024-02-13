093623b1cb6feec18cba1e5fc945ae8dffa891c9
smohan77 <sbhagavathi@gmail.com>
2024-02-13 18:25:32 +0000

    more changes

---
+++ src/main/java/com/nextsuperthing/maven/classes/Challenge_2i.java
@@ -0,0 +1,19 @@
+package com.nextsuperthing.maven.classes;
+
+public class Challenge_2i {
+	
+	 public static void main(String args[]) {
+		 
+		 //int pairCount = findPairsToMakeSum(sortedArray, X);
+		 
+	 }
+	 
+	 public static int findSolution(int[] arrayOfNumbers, int sumToMake) {
+		 int val=0;
+		 
+		 return val;
+		 
+	 }
+	 
+
+}
