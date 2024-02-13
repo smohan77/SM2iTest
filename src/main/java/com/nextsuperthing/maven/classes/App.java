7dd7cb6420bbb9fea8c8ba86968a7f8c7074357d
smohan77 <sbhagavathi@gmail.com>
2024-02-13 16:11:18 +0000

    new project

---
+++ src/main/java/com/nextsuperthing/maven/classes/App.java
@@ -0,0 +1,16 @@
+package com.nextsuperthing.maven.classes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        int n1 = 225, n2 = 115, sum;  
+        sum = n1 + n2;  
+        System.out.println("The sum of numbers is: "+sum);  
+    }
+}
